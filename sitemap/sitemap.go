4bd95b1799daed6e066fca740f27419eca6ea10a
SimonJuba <scimail09@gmail.com>
2021-09-08 22:21:57 +0900

    hacky solution to queue

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -1,12 +1,18 @@
 package sitemap
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/smelton01/go-basics/link"
 )
+const queueSize = 100
 
 type node struct {
 	url string
@@ -17,36 +23,51 @@ type node struct {
 type Map struct {
 	rootURL string
 	visitedURL map[string]bool
-	siteMap node
+	toVisit chan *node
+	siteMapRoot node
 
 }
 
 func SiteMap(rootURL string){
-	// u, err := url.parse(rootURL)
-	// CheckError(err)
 	fmt.Println("Going throu urlz")
+	u, err := url.Parse(rootURL)
+	CheckError(err)
 
 	sitemap := Map{
-		rootURL: rootURL,
+		rootURL: u.Hostname(),
 		visitedURL: make(map[string]bool),
-		siteMap: node{
+		toVisit: make(chan *node, queueSize),
+		siteMapRoot: node{
 			url: rootURL,
 			level: 0,
 			children: []node{},
 		},
 	}
 
-	children, err := sitemap.getChildren()
-	CheckError(err)
-	// fmt.Println(children)
+	sitemap.toVisit <- &sitemap.siteMapRoot
 
-	sitemap.addChildren(&sitemap.siteMap, children)
+	go sitemap.timer()
+
+	for nextNode := range sitemap.toVisit {
+		// nextNode := <-sitemap.toVisit
+		fmt.Println(nextNode.url)
+		children, err := sitemap.getChildren(nextNode.url)
+		CheckError(err)
+
+		sitemap.addChildren(nextNode, children)
+	}
 
 	fmt.Println(sitemap)
 }
 
-func (m *Map) getChildren() ([]string, error) {
-	resp, err := http.Get(m.rootURL)
+func (m *Map) timer(){
+	time.Sleep(time.Duration(15)*time.Second)
+	fmt.Println("Times up!!!!")
+	close(m.toVisit)
+}
+
+func (m *Map) getChildren(parentURL string) ([]string, error) {
+	resp, err := http.Get(parentURL)
 	CheckError(err)
 
 	defer resp.Body.Close()
@@ -65,20 +86,56 @@ func (m *Map) getChildren() ([]string, error) {
 }
 
 func (m *Map) addChildren(currNode *node, children []string){
-	for _,child := range children {
-		// re := regexp.MustCompile(m.rootURL)
+	// Add all valid children of node note yet visited to the node and add the node to toVisit list
+	if len(children) == 0 {
+		return
+	}
 
-		if _,ok := m.visitedURL[child]; ok {
+	for _, child := range children {
+
+		fullURL, err := m.checkURL(child)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if _,ok := m.visitedURL[fullURL]; ok {
 			continue
 		}
 
 		currNode.children = append(currNode.children, node{
-			url: child,
+			url: fullURL,
 			level: currNode.level + 1,
 			children: []node{},
 		})
+		// mark URL as viisted
 		m.visitedURL[child] = true
+		
+		if currNode.level > 3 {
+			log.Println("Max depth reached")
+			return
+		}
+		// add chiled node to visit queue
+		m.toVisit <- &currNode.children[len(currNode.children)-1]
 	}
+	
+}
+
+func (m Map) checkURL(URL string) (string, error) {
+	// Check if provided url is of valid format and in same domain as root url
+	if strings.HasPrefix(URL, "http") && strings.Contains(URL, func (root string) string {
+		if strings.HasPrefix(root,"www")  {
+			return root[4:]
+		}
+		return root
+	}(m.rootURL)) {
+		return URL, nil
+	}
+	if URL[0] == '/' {
+		return "https://" + m.rootURL + URL, nil
+	}
+
+	return "", errors.New("Invalid URL: " + URL)
 }
 
 func CheckError(err error){
