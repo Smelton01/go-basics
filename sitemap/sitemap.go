312326cdb0d79c22245e0ff2af803c404b5a1ecf
SimonJuba <scimail09@gmail.com>
2021-09-08 18:08:52 +0900

    added first layer

--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -3,27 +3,86 @@ package sitemap
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/smelton01/go-basics/link"
 )
 
+type node struct {
+	url string
+	level int
+	children []node
+}
 
-func SiteMap(url string){
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+type Map struct {
+	rootURL string
+	visitedURL map[string]bool
+	siteMap node
+
+}
+
+func SiteMap(rootURL string){
+	// u, err := url.parse(rootURL)
+	// CheckError(err)
+	fmt.Println("Going throu urlz")
+
+	sitemap := Map{
+		rootURL: rootURL,
+		visitedURL: make(map[string]bool),
+		siteMap: node{
+			url: rootURL,
+			level: 0,
+			children: []node{},
+		},
 	}
+
+	children, err := sitemap.getChildren()
+	CheckError(err)
+	// fmt.Println(children)
+
+	sitemap.addChildren(&sitemap.siteMap, children)
+
+	fmt.Println(sitemap)
+}
+
+func (m *Map) getChildren() ([]string, error) {
+	resp, err := http.Get(m.rootURL)
+	CheckError(err)
+
 	defer resp.Body.Close()
 
 	html, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-	layer1 := link.LinkFunc(html)
+	CheckError(err)
 
-	for _, pair := range layer1 {
-		fmt.Println(pair)
+	children := link.LinkFunc(html)
+	childrenURL := []string{}
+
+	for _, pair := range children {
+		childrenURL = append(childrenURL, pair.Href)
+	}
+
+	return childrenURL, nil
+}
+
+func (m *Map) addChildren(currNode *node, children []string){
+	for _,child := range children {
+		// re := regexp.MustCompile(m.rootURL)
+
+		if _,ok := m.visitedURL[child]; ok {
+			continue
+		}
+
+		currNode.children = append(currNode.children, node{
+			url: child,
+			level: currNode.level + 1,
+			children: []node{},
+		})
+		m.visitedURL[child] = true
+	}
+}
+
+func CheckError(err error){
+	if err != nil {
+		panic(err)
 	}
 }
